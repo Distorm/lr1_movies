a6f60393e64f2c91551ca2c03627f8801f1fc6fb
Distorm_nox <oreshnikov@bk.ru>
2024-03-13 17:13:20 +0700

    add retrofit and create interface

---
+++ app/src/main/java/com/example/lr1_movies/API/APImovie.kt
@@ -0,0 +1,7 @@
+package com.example.lr1_movies.API
+
+import retrofit2.http.GET
+
+interface APImovie {
+   // @GET("")
+}
--- app/src/main/java/com/example/lr1_movies/RVdataItem.kt
+++ app/src/main/java/com/example/lr1_movies/RVdataItem.kt
@@ -1,8 +1,7 @@
 package com.example.lr1_movies
 
-import java.util.Date
 
-class RVdataItem (
+data class RVdataItem (
     val it_image_movies: Int,
     val it_name: String,
     val it_date: String,
